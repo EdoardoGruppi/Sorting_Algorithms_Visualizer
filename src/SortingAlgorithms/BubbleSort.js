d2bba48ae0518ed8eb98fe675a2eead3fd78e0f5
EdoardoGruppi <edoardo.gruppi.20@ucl.ac.uk>
2021-08-31 23:05:48 +0200

    Add comments

--- src/SortingAlgorithms/BubbleSort.js
+++ src/SortingAlgorithms/BubbleSort.js
@@ -1,9 +1,12 @@
 export function performBubbleSort(array) {
   const animations = [];
   let check = true;
+  // Perform this loop until no swap occurs in the entire while body
   while (check) {
+    // Variable useful to understand when two items swap
     check = false;
     for (let idx = 0; idx < array.length - 1; idx++) {
+      // The swap happens only if an element is greater than that one position after
       if (array[idx] > array[idx + 1]) {
         animations.push([idx, idx + 1, 1]);
         let temp = array[idx];
@@ -15,8 +18,5 @@ export function performBubbleSort(array) {
       }
     }
   }
-
-  console.log(array);
-
   return animations;
 }
--- src/SortingAlgorithms/MergeSort.js
+++ src/SortingAlgorithms/MergeSort.js
@@ -3,15 +3,19 @@ export function performMergeSort(array) {
   // Base case or terminating case
   if (array.length < 2) return array;
   const arrayCopy = array.slice();
+  // Otherwise divide the array into two parts
   mergeSortHelper(array, 0, array.length - 1, arrayCopy, animations);
   return animations;
 }
 
 function mergeSortHelper(mainArray, start, end, arrayCopy, animations) {
+  // Return if a given array has only one value
   if (start === end) return;
   const middle = Math.floor((start + end) / 2);
+  // Divide and operate recursively on the two halves of the passed array
   mergeSortHelper(arrayCopy, start, middle, mainArray, animations);
   mergeSortHelper(arrayCopy, middle + 1, end, mainArray, animations);
+  // Merge the two subarrays
   merge(arrayCopy, start, middle, end, mainArray, animations);
 }
 
@@ -19,6 +23,8 @@ function merge(arrayCopy, start, middle, end, mainArray, animations) {
   let k = start;
   let i = start;
   let j = middle + 1;
+  // Compare the two subarrays and push the smaller element in the first position of the two arrays
+  // Do so until one of the two arrays has no items
   while (i <= middle && j <= end) {
     animations.push([i, j, 0]);
     if (arrayCopy[i] <= arrayCopy[j]) {
@@ -29,6 +35,8 @@ function merge(arrayCopy, start, middle, end, mainArray, animations) {
       mainArray[k++] = arrayCopy[j++];
     }
   }
+  // Push the remaining elements from one of the two array
+  // The while loop on the empty array is simply skipped
   while (i <= middle) {
     animations.push([k, arrayCopy[i], 1]);
     mainArray[k++] = arrayCopy[i++];
